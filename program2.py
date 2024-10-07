eb81a939f4429b24e508e31c082f5e4991a576aa
vinit dubey <vinitthedubey@gmail.com>
2024-10-07 15:31:17 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        roman_map = {
+        roman_data = {
             'I': 1,
             'V': 5,
             'X': 10,
