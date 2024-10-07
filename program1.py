54425f8d9d36995ab53c54ed429c40b747fafaa1
vinit dubey <vinitthedubey@gmail.com>
2024-10-07 15:41:25 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -19,23 +19,7 @@ class Solution(object):
 
 class Solution(object):
     def isValid(self, s):
-        stack = []
-        bracket_format = {')': '(', '}': '{', ']': '['}
         
-        for char in s:
-            if char in bracket_format:
-                if stack:
-                    top = stack.pop()
-                else:
-                    top = None
-                
-                if bracket_format[char] != top:
-                    return False
-            else:
-                stack.append(char)
-        
-        return not stack
-
 
 
 
