ea78789f69e4b9fb9250e76f9ae0ca93dbce7d71
vinit dubey <vinitthedubey@gmail.com>
2024-10-07 15:41:14 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: bool
         """
         stack = []
-        bracket_format = {')': '(', '}': '{', ']': '['} 
+        bracket_format = {')': '(', '}': '{', ']': '['} #dictionary with Closing form as key and open as value
         
         for char in s:
             if char in bracket_format:
@@ -20,7 +20,7 @@ class Solution(object):
 class Solution(object):
     def isValid(self, s):
         stack = []
-        bracket_format = {')': '(', '}': '{', ']': '['} #dictionary with Closing form as key and open as value
+        bracket_format = {')': '(', '}': '{', ']': '['}
         
         for char in s:
             if char in bracket_format:
