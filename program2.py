22e74c24f967f9036fd4d8640a1cbd1370cab85f
vinit dubey <vinitthedubey@gmail.com>
2024-10-07 16:11:00 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -14,14 +14,15 @@ class Solution(object):
             'M': 1000
         }
         
-        total_sum = 0 #by default value is 0 as empty string may also come.
+        total_sum = 0  # Default value is 0 as empty string may also come.
         length = len(s)
         
         for i in range(length):
             num_value = roman_data[s[i]]
+            # Check if this is not the last character and the current value is less than the next one
             if i + 1 < length and num_value < roman_data[s[i + 1]]:
-                total_sum -= num_value
+                total_sum -= num_value  # Subtract if less than the next
             else:
-                total_sum += num_value
+                total_sum += num_value  # Otherwise, add
         
         return total_sum
